e17d3b512991e5b8a02a1f60e5736ba13b48ca50
ROGERIO SOUSA (MDM) <sousarogerio349@gmail.com>
2022-09-12 21:32:40 -0300

    if

--- aula01.py
+++ aula01.py
@@ -1,4 +1,18 @@
 print("ola back")
 a=10
 b=5 
-c= a+b
+c=a+b
+print(c)
+
+# O QUE DEVO FAZER QUANDO ACORDAR?
+devo_levantar = False
+esta_muito_frio = True
+esta_muito_quente = True
+if devo_levantar == True:
+    print('levantar e preparar o café(extra forte)')
+elif esta_muito_frio == True:
+    print('ficar quientinho na cama!')
+elif esta_muito_quente == True:
+    print('Aumentar o ventilador!')
+else:
+    print('dormir mais um tanto!')
